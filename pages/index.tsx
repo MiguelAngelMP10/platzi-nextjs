234d06dd4e00532dbe5d4065e5369d4a915d1258
MiguelAngelMP10 <mmunozpozos@gmail.com>
2020-08-19 15:55:42 -0500

    Creando y consumiendo nuestra propia API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Link from 'next/link'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState([])
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+        setProductList(data)
+      })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map((product, index) => {
+        return (
+          <React.Fragment key={index}>
+            <Link href="/product/[id]" as={`/product/${product.id}`}>
+              Ver
+            </Link>
+            <h1>{product.id}</h1>
+            <div>{product.name}</div>
+            <img src={product.image} alt="imagen" />
+          </React.Fragment>
+        )
+      })}
     </div>
   )
 }
